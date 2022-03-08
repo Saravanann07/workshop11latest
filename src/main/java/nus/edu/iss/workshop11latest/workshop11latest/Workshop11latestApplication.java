389ed5781066ddd9c625c04145aa62f65b386c04
Saravanan Munusamy <saravananmunusamy@Saravanans-MacBook-Pro.local>
2022-03-08 19:01:01 +0800

    new

---
+++ src/main/java/nus/edu/iss/workshop11latest/workshop11latest/Workshop11latestApplication.java
@@ -0,0 +1,13 @@
+package nus.edu.iss.workshop11latest.workshop11latest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop11latestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshop11latestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nus/edu/iss/workshop11latest/workshop11latest/Workshop11latestApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.edu.iss.workshop11latest.workshop11latest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop11latestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
